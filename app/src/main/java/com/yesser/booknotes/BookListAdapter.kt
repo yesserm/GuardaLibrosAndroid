b4b055eadb740f201c83d0c2c6e4f742f98fbc16
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2020-12-11 14:34:12 -0600

    Crear el adaptador para el recyclerView

---
+++ app/src/main/java/com/yesser/booknotes/BookListAdapter.kt
@@ -0,0 +1,44 @@
+package com.yesser.booknotes
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class BookListAdapter (private val context: Context) : RecyclerView.Adapter<BookListAdapter.BookViewHolder>() {
+
+    private var bookList: List<Book> = mutableListOf()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BookListAdapter.BookViewHolder {
+        val itemView = LayoutInflater.from(context).inflate(R.layout.list_item,parent, false)
+        return BookViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: BookListAdapter.BookViewHolder, position: Int) {
+        val book = bookList[position]
+        holder.setData(book.author, book.book, position)
+    }
+
+    override fun getItemCount(): Int = bookList.size
+
+    fun setBooks(books: List<Book>) {
+        bookList = books
+        notifyDataSetChanged()
+    }
+
+    inner class BookViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        private var pos: Int = 0
+
+        fun setData(author: String, book: String, position: Int){
+            itemView.tvAuthor.text = author
+            itemView.tvBook.text = book
+            this.pos = position
+        }
+    }
+}
